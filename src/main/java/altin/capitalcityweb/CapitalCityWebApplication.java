639cf198ef5d4f47637c7056cadf1a869954e7ad
ALTINLAPTOP\User <wi21b054@technikum-wien.at>
2022-06-21 13:30:07 +0200

    Initial commit

---
+++ src/main/java/altin/capitalcityweb/CapitalCityWebApplication.java
@@ -0,0 +1,13 @@
+package altin.capitalcityweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalCityWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalCityWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/altin/capitalcityweb/CapitalCityWebApplicationTests.java
@@ -0,0 +1,13 @@
+package altin.capitalcityweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalCityWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
